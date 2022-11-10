722a719e1009c731f443c3747064ecdb452c4fbc
Aelanos <gen235@gmail.com>
2022-11-10 00:13:33 -0300

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,18 @@
+//ejercicio 2
+
+const Libro = {
+    Titulo: "El Principito",
+    Autor: "Antoine de Saint-Exupéry",
+    Fecha: 1943,
+    Url: "https://books.google.cl/books/about/El_Principito.html?id=x9YBEAAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es-419&redir_esc=y#v=onepage&q&f=false"
+}
+const ejercicio = ["Jose Manuel Agar",
+29, 
+false, 
+new Date("november 02 1993"),
+Libro
+];
+
+console.log(ejercicio)
+
+
