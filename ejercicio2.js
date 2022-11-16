44d05dacb9a1a334b27c653f297f875f2586477d
Aelanos <gen235@gmail.com>
2022-11-16 01:41:17 -0300

    format

--- ejercicio2.js
+++ ejercicio2.js
@@ -7,10 +7,10 @@ const Libro = {
     Url: "https://books.google.cl/books/about/El_Principito.html?id=x9YBEAAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es-419&redir_esc=y#v=onepage&q&f=false"
 }
 const ejercicio = ["Jose Manuel Agar",
-29, 
-false, 
-new Date("november 02 1993"),
-Libro
+    29,
+    false,
+    new Date("november 02 1993"),
+    Libro
 ];
 
 console.log(ejercicio)
