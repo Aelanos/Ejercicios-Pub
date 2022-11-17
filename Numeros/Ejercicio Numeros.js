b35deab9105ff39046cb98c7b2938fb060897073
Aelanos <gen235@gmail.com>
2022-11-17 20:10:14 -0300

    asd

---
+++ Numeros/Ejercicio Numeros.js
@@ -0,0 +1,16 @@
+// Ejercicio 
+
+let a = 181
+let Peso_kg = 100.2
+
+let Altura_m = parseInt(a) / 100
+let Altura_cm = parseFloat(a)
+let valor_maximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
+console.log(Altura_m);
+console.log(Altura_cm);
+console.log(Peso_kg);
+console.log(Math.ceil(Altura_m));
+console.log(Math.floor(Peso_kg));
+console.log(valor_maximo);
