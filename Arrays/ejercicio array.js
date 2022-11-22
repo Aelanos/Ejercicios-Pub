27337edcae0521ac298923d72f39492ce4d1bae3
Jose Manuel Agar <gen235@gmail.com>
2022-11-22 19:56:34 -0300

    cambio fecha

--- Arrays/ejercicio array.js
+++ Arrays/ejercicio array.js
@@ -1,22 +1,26 @@
 // Ejercicio arrays
-
+// Lista Compra
 const listaCompra = ["pan", "huevos", "arroz", "leche", "carne"]
 console.log(listaCompra);
 
 listaCompra.push("aceite de girasol")
 
 console.log(listaCompra);
-
+// Peliculas 
 const peliculasFav = [
-    { Titulo: "harry Potter y la piedra Filosofal", Director:"J. K. Rowling", Fecha: new Date ("November 29 2001") },
-    { Titulo: "Happy Feet", Director:"George Miller", Fecha: new Date ("December 1 2006") },
-    { Titulo: "School of Rock" , Director:"Richard Linklater", Fecha: new Date ("February 13 2004")}
+    { Titulo: "harry Potter y la piedra Filosofal", Director: "J. K. Rowling", Fecha: new Date("November 29 2001") },
+    { Titulo: "Happy Feet", Director: "George Miller", Fecha: new Date("December 1 2006") },
+    { Titulo: "School of Rock", Director: "Richard Linklater", Fecha: new Date("February 13 2004") }
 ]
 
-const Directores = peliculasFav.map(peliculasFav =>{
+const fechaPost = peliculasFav.filter(peliculasFav => peliculasFav.Fecha > new Date("january 1 2010"))
+console.log(fechaPost);
+
+// Concatenacion 
+const Directores = peliculasFav.map(peliculasFav => {
     return peliculasFav.Director
 })
-const Peliculas = peliculasFav.map(peliculasFav =>{
+const Peliculas = peliculasFav.map(peliculasFav => {
     return peliculasFav.Titulo
 })
 
@@ -27,5 +31,5 @@ const dirTit_Concat = Directores.concat(Peliculas)
 console.log(dirTit_Concat);
 
 
-const dirTit_prop = [...Directores,...Peliculas]
+const dirTit_prop = [...Directores, ...Peliculas]
 console.log(dirTit_prop);
