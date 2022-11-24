3a9c19339f47562cef8013d99d48146d50b8b33e
Jose Manuel Agar <gen235@gmail.com>
2022-11-24 15:06:54 -0300

    ejercicio set familia

---
+++ Ejercicios Sets/conjuntos.js
@@ -0,0 +1,17 @@
+//ejercicio Objetos
+
+const familieNames = ["Loreto", "Fernando", "Javiera", "Feña", "José"]
+const miSet = new Set(familieNames)
+
+console.log(miSet);
+
+miSet.add("José")
+
+console.log(miSet);
+miSet.add("JavaScript")
+
+console.log(miSet);
+
+
+
+
